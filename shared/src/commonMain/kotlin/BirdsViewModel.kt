24392a64ad3157c59218809650608335ef5ddca2
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-10-24 18:21:03 -0500

    Impl() - Implement httpClient to get images. -

--- shared/src/commonMain/kotlin/BirdsViewModel.kt
+++ shared/src/commonMain/kotlin/BirdsViewModel.kt
@@ -1,6 +1,13 @@
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import model.BirdImage
 
 data class BirdsUiState(
@@ -8,6 +15,37 @@ data class BirdsUiState(
 )
 
 class BirdsViewModel : ViewModel() {
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+
+    init {
+        updateImages()
+    }
+
+    override fun onCleared() {
+        httpClient.close()
+    }
+
+    fun updateImages() {
+        viewModelScope.launch {
+            val images = getImages()
+            _uiState.update {
+                it.copy(image = images)
+            }
+        }
+    }
+
+    private suspend fun getImages(): List<BirdImage> {
+        val images = httpClient
+            .get("https://sebi.io/demo-image-api/pictures.json")
+            .body<List<BirdImage>>()
+        return images
+    }
+
+
     private val _uiState = MutableStateFlow<BirdsUiState>(BirdsUiState())
     val uiState = _uiState.asStateFlow()
 
